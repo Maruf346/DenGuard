08823518a8b8aa33559d5fa934f11fb7fdd58916
Maruf Hossain <maruf.bshs@gmail.com>
2025-08-14 22:37:20 +0600

    Add environment variable for TOMORROW_API_KEY and set timezone

--- denguard/denguard/settings.py
+++ denguard/denguard/settings.py
@@ -9,11 +9,17 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# make sure time is correct for “today”
+TIME_ZONE = "Asia/Dhaka"
+USE_TZ = True
+
+TOMORROW_API_KEY = os.environ.get("TOMORROW_API_KEY", "")
 
 
 # Quick-start development settings - unsuitable for production
